961c5ebbc4d5367dc3d2549da9459a657e8d85ce
ssg192 <129010819+ssg192@users.noreply.github.com>
2023-11-29 08:58:48 -0600

    Update interfaz.c

--- interfaz.c
+++ interfaz.c
@@ -138,7 +138,7 @@ void cliente(int shmid, int semid) {
 int main() {
     initscr();
 
-    key_t key = ftok("shmfile", 65);
+    key_t key = ftok("Archvivo",'k');
     int shmid = shmget(key, SHM_SIZE, 0666 | IPC_CREAT);
 
     if (shmid == -1) {
